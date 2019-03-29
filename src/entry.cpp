8dba457584e552e4fbc725dc89fc44e86cb19767
striek <striekcarl@hotmail.com>
2019-03-29 11:58:34 +0100

    added deriative and newton-raphson

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "expression.hpp"
 
-double test(double x)
+double test(double x) noexcept
 {
 	// x^3 - x - 2
 	return std::pow(x, 3) - x - 2;
 }
 
-int main()
+int main() noexcept
 {
-	const auto root = expression<test>().bisection(std::make_pair(1.0, 2.0));
-
-	std::printf("Root: [%f]\n", root);
+	std::printf("Deriative: [%f]\n", expression<test>().deriative(0.0));
+	std::printf("Bisection: [%f]\n", expression<test>().bisection(std::make_pair(1.0, 2.0)));
+	std::printf("Newton Raphson: [%f]\n", expression<test>().newton_raphson(5.0));
 }
--- src/expression.hpp
+++ src/expression.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include <type_traits>
 #include <utility>
+#include <limits>
 #include <math.h>
 
 template <auto Fn>
 class expression
 {
 public:
+	// EVALUATE THE EXPRESSION WITH AN ARBITRARY VARIABLE
 	template <class T>
 	auto evaluate(const T value) 
 		const noexcept -> decltype(Fn(value))
@@ -14,43 +16,69 @@ public:
 		return Fn(value);
 	}
 
+	// CALCULATE DERIATIVE
 	template <class T>
-	auto bisection(const std::pair<T, T> interval) 
+	auto inline deriative(const T value) 
+		const noexcept -> decltype(Fn(value))
+	{
+		// SMALLEST POSSIBLE CHANGE IN A DOUBLE
+
+		// REMEMBER TO DOCUMENT THIS
+		const auto delta = std::sqrt( std::numeric_limits<double>::epsilon() ); // 1.49011611938476562e-8
+
+		// CALCULATE SLOPE
+		return (Fn(value + delta) - Fn(value)) / delta;
+	}
+
+	// NEWTON-RAPHSON
+	template <class T>
+	auto inline newton_raphson(const T value)
+		const noexcept -> decltype(Fn(value))
+	{
+		// CALCULATE NEW VALUE
+		const auto new_value = value - (Fn(value) / this->deriative(value));
+
+		auto has_acceptable_precision = [](T x, T y) -> T
+		{
+			constexpr auto precision = 0.00000001; // DETERMINES EQUALITY PRECISION
+			return std::abs(x - y) < precision;
+		};
+
+		if (has_acceptable_precision(value, new_value))
+			return new_value;
+
+		// NOT IMPLEMENTED
+		return this->newton_raphson(new_value);
+	}
+
+	// BISECTION ANALYSIS
+	template <class T>
+	auto inline bisection(std::pair<T, T> interval) 
 		const noexcept -> decltype(Fn(interval.first))
 	{
-		// COPY INTERVAL VALUES AND UNPACK USING STRUCTURED BINDINGS
-		auto [begin, end] = interval;
+		// UNPACK INTERVAL USING STRUCTURED BINDINGS
+		auto& [begin, end] = interval;
 
-		while (true)
+		// CALCULATE MIDPOINT
+		const auto mid_point = (begin + end) / 2.0;
+		const auto mid = Fn(mid_point);
+
+		// FOUND ROOT?
+		constexpr auto precision = 0.00000001; // DECREASE TO INCREASE PRECISION-LEEWAY
+		if (std::abs(mid) < precision)
+			return mid_point;
+
+		// CHECK SIGN CHANGE AND UPDATE INTERVAL
+		const auto begin_eval = Fn(begin);
+		if (std::signbit(begin_eval) == std::signbit(mid))
 		{
-			// CALCULATE MIDPOINT
-			const auto mid_point = (begin + end) / 2.0;
-			
-			// CHECK PRECISION
-			constexpr auto convergence = 0.1; // DECREASE TO INCREASE PRECISION-LEEWAY
-			if (std::abs(mid_point - begin) < convergence)
-				break; // MID POINT WAS NEVER FOUND
-
-			// CHECK SIGN CHANGE
-			const auto mid_point_evaluation = Fn(mid_point);
-
-			// FOUND ROOT?
-			if (mid_point_evaluation == 0.00)
-				return mid_point;
-
-			// UPDATE INTERVAL
-			const auto begin_evaluation = Fn(begin);
-			if (std::signbit(begin_evaluation) == std::signbit(mid_point_evaluation))
-			{
-				begin = mid_point;
-			}
-			else
-			{
-				end = mid_point;
-			}
-
+			begin = mid_point;
 		}
-		
-		return T{INFINITY};
+		else
+		{
+			end = mid_point;
+		}
+
+		return this->bisection<T>(interval);
 	}
 };
