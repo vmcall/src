79c7c6cfaaf9da4dab900d3c17e7fa4ec71b2557
striek <striekcarl@hotmail.com>
2019-04-04 12:02:47 +0200

    a

--- src/analysis.hpp
+++ src/analysis.hpp
@@ -12,19 +12,85 @@ public:
 	static inline auto first()
 		noexcept -> void
 	{
-		std::printf("First\n");
+		std::printf("Testing x^2\n");
 
-		const auto expr_1 = expression< test_case::one<my_precision_t> >();
-		const auto clock = timer([&](const my_precision_t val) -> my_precision_t
+		const auto expr = expression<test_case::one<my_precision_t>>();
+
+		// TIME NEWTON RAPHSON
+		const auto[time_newton_raphson, result_newton_raphson] = timer(
+			[&](const my_precision_t val) 
+				-> my_precision_t
 		{
-			return val;
-		});
+			return expr.newton_raphson(val);
+		}).time<std::chrono::nanoseconds>(1.00);
 
-		const auto[a, b] = clock.time<std::chrono::nanoseconds>(15.0);
+		// TIME BISECTION
+		const auto[time_bisection, result_bisection] = timer(
+			[&](const std::pair<my_precision_t, my_precision_t> val) 
+				-> my_precision_t
+		{
+			return expr.bisection(val);
+		}).time<std::chrono::nanoseconds>( std::make_pair(-6.0, 2.0) );
 
-		std::printf("%lld - %f\n", a.count(), b);
+		// PRINT
+		std::printf(" [Bisection]      %.10f - Took %lld ns\n", result_bisection, time_bisection.count());
+		std::printf(" [Newton-Raphson] %.10f - Took %lld ns\n", result_newton_raphson, time_newton_raphson.count());
+	}
 
-		std::printf(" [Bisection]      %.10f\n", expr_1.bisection(std::make_pair(-1.0, 1.0)));
-		std::printf(" [Newton-Raphson] %.10f\n", expr_1.newton_raphson(1.0));
+
+	static inline auto second()
+		noexcept -> void
+	{
+		std::printf("Testing x^3 - x - 2\n");
+
+		const auto expr = expression<test_case::two<my_precision_t>>();
+
+
+		for (double i = 1.0; i < 100; i++)
+		{
+			// TIME NEWTON RAPHSON
+			const auto[time_newton_raphson, result_newton_raphson] = timer(
+				[&](const my_precision_t val)
+				-> my_precision_t
+			{
+				return expr.newton_raphson(val);
+			}).time<std::chrono::nanoseconds>(i);
+
+			std::printf("%.0f;%lld\n", i, time_newton_raphson.count());
+		}
+
+
+
+		// TIME BISECTION
+		const auto[time_bisection, result_bisection] = timer(
+			[&](const std::pair<my_precision_t, my_precision_t> val)
+			-> my_precision_t
+		{
+			return expr.bisection(val);
+		}).time<std::chrono::nanoseconds>(std::make_pair(1.0, 2.0));
+
+		// PRINT
+		std::printf(" [Bisection]      %.10f - Took %lld ns\n", result_bisection, time_bisection.count());
+		//std::printf(" [Newton-Raphson] %.10f - Took %lld ns\n", result_newton_raphson, time_newton_raphson.count());
+	}
+
+	static inline auto third()
+		noexcept -> void
+	{
+		std::printf("Testing cbroot(x)\n");
+
+		const auto expr = expression<test_case::three<my_precision_t>>();
+
+		// TIME BISECTION
+		const auto[time_bisection, result_bisection] = timer(
+			[&](const std::pair<my_precision_t, my_precision_t> val)
+			-> my_precision_t
+		{
+			return expr.bisection(val);
+		}).time<std::chrono::nanoseconds>(std::make_pair(-1.0, 1.0));
+
+
+		// PRINT
+		std::printf(" [Bisection]      %.10f - Took %lld ns\n", result_bisection, time_bisection.count());
 	}
 };
--- src/entry.cpp
+++ src/entry.cpp
@@ -5,27 +5,4 @@ int main() noexcept
 	analysis::first();
 	analysis::second();
 	analysis::third();
-
-	// VALUE CHECK
-
-	// ONE
-
-	
-	
-
-	// TWO
-	std::printf("Two\n");
-	std::printf(" [Bisection]      %.10f\n", expression< test_case::two<my_precision_t> >().bisection(std::make_pair(1.0, 2.0)));
-	std::printf(" [Newton-Raphson] %.10f\n", expression< test_case::two<my_precision_t> >().newton_raphson(1.0));
-	
-	// THREE
-	std::printf("Three\n");
-	std::printf(" [Bisection]      %.10f\n", expression< test_case::three<my_precision_t> >().bisection(std::make_pair(-1.0, 1.0)));
-
-	// NOT SUPPORTED
-	// std::printf(" [Newton-Raphson] %.10f\n",	expression< test_case::three<my_precision_t> >().newton_raphson(1.0));
-
-	//const auto [val, result] = timer< test_case::one<my_precision_t> >().time<std::chrono::nanoseconds>(15.0);
-	//std::cout << val.count() << std::endl;
-	//std::cout << result << std::endl;
 }
